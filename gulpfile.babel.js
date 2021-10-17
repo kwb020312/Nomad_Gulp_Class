b1d6fdb280cf40cf6cabe6b07695dcfafe9d9da6
kwb020312 <kwb020312@naver.com>
2021-10-17 12:57:45 +0900

    #4.0 Gulp SASS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,8 @@ import del from 'del';
 import ws from 'gulp-webserver';
 import image from 'gulp-image';
 
+const sass = require('gulp-sass')(require('node-sass'));
+
 const routes = {
 	pug: {
 		src: 'src/*.pug',
@@ -13,6 +15,11 @@ const routes = {
 	img: {
 		src: 'src/img/*',
 		dest: 'build/img'
+	},
+	scss: {
+		src: 'src/scss/style.scss',
+		dest: 'build/css',
+		watch: 'src/scss/**/*.scss'
 	}
 };
 
@@ -20,18 +27,22 @@ const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pu
 
 const clean = () => del([ 'build' ]);
 
-const webserver = () => gulp.src('build').pipe(ws({ livereload: true, open: true }));
+const webserver = () => gulp.src('build').pipe(ws({ livereload: true }));
 
 const watch = () => {
 	gulp.watch(routes.pug.watch, pug);
 	gulp.watch(routes.pug.watch, img);
+	gulp.watch(routes.scss.watch, styles);
 };
 
 const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
+const styles = () =>
+	gulp.src(routes.scss.src).pipe(sass().on('error', sass.logError)).pipe(gulp.dest(routes.scss.dest));
+
 const prepare = gulp.series([ clean, img ]);
 
-const assets = gulp.series([ pug ]);
+const assets = gulp.series([ pug, styles ]);
 
 const postDev = gulp.series([ webserver, watch ]);
 
