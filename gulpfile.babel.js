5e83e715c32a7999d996daadaaba8031e5db54d9
kwb020312 <kwb020312@naver.com>
2021-10-17 12:17:49 +0900

    #3.0 Image Optimization

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,38 @@
 import gulp from 'gulp';
 import gpug from 'gulp-pug';
+import del from 'del';
+import ws from 'gulp-webserver';
+import image from 'gulp-image';
 
 const routes = {
 	pug: {
 		src: 'src/*.pug',
-		dest: 'build'
+		dest: 'build',
+		watch: 'src/**/*.pug'
+	},
+	img: {
+		src: 'src/img/*',
+		dest: 'build/img'
 	}
 };
 
-export const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-export const dev = gulp.series([ pug ]);
+const clean = () => del([ 'build' ]);
+
+const webserver = () => gulp.src('build').pipe(ws({ livereload: true, open: true }));
+
+const watch = () => {
+	gulp.watch(routes.pug.watch, pug);
+	gulp.watch(routes.pug.watch, img);
+};
+
+const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+
+const prepare = gulp.series([ clean, img ]);
+
+const assets = gulp.series([ pug ]);
+
+const postDev = gulp.series([ webserver, watch ]);
+
+export const dev = gulp.series([ prepare, assets, postDev ]);
